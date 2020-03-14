be63e28d4469fbcda6aaed4c2cc913b6223d9d72
msaaddev <mrsaadirfan@gmail.com>
2020-03-14 12:18:56 +0500

    📖 DOC: commenting

--- js/zoom.js
+++ js/zoom.js
@@ -5,15 +5,26 @@
 	Website: https://msaad.dev
 */
 
+/*
+	A function that will decide the API namespace according to the browser extension is running in.
+*/
 window.browser = (function() {
 	return window.browser || window.chrome;
 })();
+
+/*
+	Getting the ids of all the five buttons in the extension
+*/
 
 const btnValue1 = document.getElementById('1');
 const btnValue2 = document.getElementById('2');
 const btnValue3 = document.getElementById('3');
 const btnValue4 = document.getElementById('4');
 const btnValue5 = document.getElementById('5');
+
+/*
+	Adding event listeners to set the zoom of the tabs
+*/
 
 btnValue1.addEventListener('click', () => {
 	chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
